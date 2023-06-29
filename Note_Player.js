536bbbc4a4783127fb0cfde03f03e8a0354f84ca
Kevin Holmes <kmh0237@gmail.com>
2023-06-29 16:14:56 -0500

    initial code finished mostly

--- Note_Player.js
+++ Note_Player.js
@@ -1,21 +1,27 @@
+let keyId = -1;
+
+console.log("init")
+window.addEventListener('load', function() {
+    console.log("load")
+    getKeys();
+})
+
+function setKeyId() {
+    keyId = keyId + 1;
+    return keyId;
+}
+
 function setClicker(key) {
-    key.addEventListener('click', () => {
-        //
-    } );
+    key.addEventListener('click', function() {
+        alert(key.id);
+    });
 }
 
-function getKeys(sound) {
+function getKeys() {
     const keys = document.getElementById('PianoKeys').querySelectorAll('div');
-
+    console.log(keys)
     keys.forEach(key => {
+        key.id = setKeyId();
         setClicker(key);
-      });
-}
-
-function stop() {
-    playedKeys.forEach(key => {
-        key.pause();
-        key.currentTime = 0;
-    })
-    playedKeys = []
+    });
 }
