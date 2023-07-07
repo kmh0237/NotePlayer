40e91007005c9748483bb9196f8ba4cb7325257b
AUMD185 <kookerdevon@johndeere.com>
2023-07-07 15:53:21 -0500

    comment touchup"

--- Note_Player.js
+++ Note_Player.js
@@ -1,24 +1,25 @@
 /*
 
-    let: Used to declare a Variable (which is a way to store information in your program)
+    let: Used to declare a Variable (which is a way to store information).
 
-    function: Declare a function (which is a way to share common code)
+    function: Declare a function (which is a way to share common code, calculate things and keep your code clean).
 
 */
 
+// keeps track of what id things will be.
 let keyId = -1;
 
 window.addEventListener('load', function() {
     getKeys();
 })
 
-// Keeping track of global variable keyid, incrementing it, and returning it
+// Keeping track of the variable "keyId", adding to it, and returning it.
 function setKeyId() {
     keyId = keyId + 1;
     return keyId;
 }
 
-// Setting the action of the key passed into it
+// Setting the action of the key it's given
 function setKeyAction(key) {
     key.addEventListener('click', function() {
         alert(key.id);
@@ -27,11 +28,11 @@ function setKeyAction(key) {
 
 // Getting the keys from the html and looping through them
 function getKeys() {
-    
-    // This gets the keys from the html, by first finding the #piano-div then getting all the divs underneath it in an array
+
+    // This gets the keys from the html by finding the "piano-div", then getting all the divs (keys) underneath it.
     const keys = document.getElementById('piano-div').querySelectorAll('div');
 
-    // Looping through the keys and setting their actions
+    // Looping through the keys and setting their actions.
     keys.forEach(key => {
         key.id = setKeyId();
         setKeyAction(key);
