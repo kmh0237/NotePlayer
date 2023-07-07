800b833d1cf363d734d48943c3f7fec0c81010b0
Kevin Holmes <kmh0237@gmail.com>
2023-07-07 16:35:47 -0500

    Added missing semicolons

--- Note_Player.js
+++ Note_Player.js
@@ -16,10 +16,10 @@ let numberOfOctaves = 1;
 let audio = new Audio('./piano_key.mp3');
 
 // Keeps track of keys which are currently playing
-let playedKeys = []
+let playedKeys = [];
 
 // Keeps track of the pitches for each key, by index
-let keyPitches = []
+let keyPitches = [];
 
 // Gets the key functions in when the page loads
 window.addEventListener('load', function() {
