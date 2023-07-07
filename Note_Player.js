e3a415e16e7ef74d7ed31bae55cb6f3434e82d7c
Kevin Holmes <kmh0237@gmail.com>
2023-07-07 15:41:46 -0500

    start of comments

--- Note_Player.js
+++ Note_Player.js
@@ -4,17 +4,20 @@ window.addEventListener('load', function() {
     getKeys();
 })
 
+// Keeping track of global variable keyid, incrementing it, and returning it
 function setKeyId() {
     keyId = keyId + 1;
     return keyId;
 }
 
+// Setting the action of the key passed into it
 function setKeyAction(key) {
     key.addEventListener('click', function() {
         alert(key.id);
     });
 }
 
+// Getting the keys from the DOM and looping through them
 function getKeys() {
     const keys = document.getElementById('piano-div').querySelectorAll('div');
     keys.forEach(key => {
