40feae47831787bc845684dc01f83eddcc903bb9
AUMD185 <kookerdevon@johndeere.com>
2023-07-07 16:15:06 -0500

    comment changes and relocating functions

--- Note_Player.js
+++ Note_Player.js
@@ -15,9 +15,23 @@ let audio = new Audio('./piano_key.mp3');
 // Keeps track of keys which are currently playing
 let playedKeys = []
 
+// Gets the key functions in when the page loads
 window.addEventListener('load', function() {
     getKeys(audio);
 })
+
+// Getting the keys from the html and looping through them
+function getKeys(sound) {
+
+    // This gets the keys from the html by finding the "piano-div", then getting all the divs (keys) underneath it.
+    const keys = document.getElementById('piano-div').querySelectorAll('div');
+
+    // Looping through the keys and setting their actions.
+    keys.forEach(key => {
+        key.id = setKeyId();
+        setKeyAction(key, sound);
+    });
+}
 
 // Keeping track of the variable "keyId", adding to it, and returning it.
 function setKeyId() {
@@ -35,20 +49,7 @@ function setKeyAction(key, sound) {
     });
 }
 
-// Getting the keys from the html and looping through them
-function getKeys(sound) {
-
-    // This gets the keys from the html by finding the "piano-div", then getting all the divs (keys) underneath it.
-    const keys = document.getElementById('piano-div').querySelectorAll('div');
-
-    // Looping through the keys and setting their actions.
-    keys.forEach(key => {
-        key.id = setKeyId();
-        setKeyAction(key, sound);
-    });
-}
-
-// Find any keys that are playing and stop them
+// Finds any keys that are playing and stop them
 function stop() {
     playedKeys.forEach(key => {
         key.pause();
