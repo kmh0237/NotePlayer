90a21e72d95df515da0510691fe62419d81f8f85
kmh0237 <kmh0237@gmail.com>
2023-07-15 14:45:25 -0500

    semicolon

--- Note_Player.js
+++ Note_Player.js
@@ -42,7 +42,7 @@ function getKeys(sound) {
 // Setting the pitches of each key, using complicated math
 function setKeyPitches(numberOfKeys) {
     for(let i = 0; i < numberOfKeys; i++) {
-        keyPitches.push(2 ** ((i - Math.floor(1 / 2) * 12) / 12))
+        keyPitches.push(2 ** ((i - Math.floor(1 / 2) * 12) / 12));
     }
 }
 
